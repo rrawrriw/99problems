c0d5fb48800d97883bfa0977d073108ff788baa9
Tim Tochtermann <tim.tochtermann@gmail.com>
2015-10-14 21:31:57 +0200

    p16

---
+++ p16.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	l := []string{"a", "b", "c", "d", "e", "f"}
+	v := drop(l, 2)
+	fmt.Println(l)
+}
+
+func drop(li []string, i int) []string {
+	if len(li)-1 < i {
+		return []string{}
+	}
+
+	l := make([]string, len(li))
+	copy(l, li)
+
+	ind := []int{}
+	lo := len(l) / i
+	for x := 1; x <= lo; x++ {
+		ind = append(ind, (x*i)-x)
+	}
+
+	for _, e := range ind {
+		l = append(l[:e], l[e+1:]...)
+	}
+
+	return l
+}
