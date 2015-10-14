dbfec17c1a1ec0c6684204b820a5bb98709aed5f
Tim Tochtermann <tim.tochtermann@gmail.com>
2015-10-14 20:27:34 +0200

    p9

---
+++ p9.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	l := []string{"a", "b", "b", "b", "a", "c", "c"}
+	r := pack(l)
+	fmt.Println(r)
+}
+
+func pack(l []string) [][]string {
+	n := [][]string{}
+	last := ""
+
+	for _, e := range l {
+		if last != e {
+			n = append(n, []string{})
+		}
+		
+		n[len(n)-1] = append(n[len(n)-1], e)
+		last = e
+	}
+
+	return n
+}
+
