836ee7f74583789b5e29a8360290727e49de9aca
Tim Tochtermann <tim.tochtermann@gmail.com>
2015-10-14 20:27:42 +0200

    p10

---
+++ p10.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	l := []string{"a", "b", "b", "b", "a", "c", "c"}
+	r := encode(l)
+	fmt.Println(r)
+}
+
+func encode(l []string) []map[string]int {
+	n := []map[string]int{}
+	last := ""
+
+	for _, e := range l {
+		if last != e {
+			n = append(n, map[string]int{})
+		}
+		
+		n[len(n)-1][e] = n[len(n)-1][e] + 1
+		last = e
+	}
+
+	return n
+}
+
